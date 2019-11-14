c692ce362382da415875d0256b05b2fbe750be59
rhy0u <jeasoncentaure@MacBook-Pro-de-Jeason.local>
2019-11-14 17:50:14 +0100

    chore: webpack configuration

--- src/server/config.js
+++ src/server/config.js
@@ -3,14 +3,29 @@ import convict from 'convict'
 const config = convict({
   env: {
     env: 'NODE_ENV',
-    default: 'development'
+    default: 'development',
   },
   server: {
     port: {
       env: 'PORT',
-      default: 8000
-    }
-  }
+      default: 8000,
+    },
+    assets: {
+      webpackAssets: {
+        doc: 'Use webpack-assets.json file',
+        format: Boolean,
+        default: false,
+        env: 'WEBPACK_ASSETS',
+      },
+      main: {
+        js: {
+          doc: 'Main JS file',
+          format: String,
+          default: '/dist/main.js',
+        },
+      },
+    },
+  },
 })
 
 config.validate()
